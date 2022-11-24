740d027d9c662390094a668112f241ebb9e43537
AlexeyIvanov1981 <lelikus81@yandex.ru>
2022-11-24 22:26:16 +0300

    add check to Main

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,18 @@ public class Main {
         MagicBox<Integer> magicBox = new MagicBox<Integer>(3);
         magicBox.add(1);
         magicBox.add(22);
-        magicBox.pick();
+        //magicBox.pick();
         magicBox.add(333);
         magicBox.add(4444);
         magicBox.pick();
 
+        MagicBox<String> magicBox2 = new MagicBox<String>(3);
+        magicBox2.add("test1");
+        magicBox2.add("test22");
+        //magicBox2.pick();
+        magicBox2.add("test333");
+        magicBox2.add("test4444");
+        magicBox2.pick();
 
     }
 }
