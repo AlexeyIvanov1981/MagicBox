ef6e95dcf1eed16b7cfc062dd26a9ac98f68bd66
AlexeyIvanov1981 <lelikus81@yandex.ru>
2022-11-24 21:27:49 +0300

    add new logic in all clases

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,27 +1,29 @@
 import RuntimeExceptionForMagicBox.IsFull;
 import RuntimeExceptionForMagicBox.IsNotFull;
 
+import java.util.Arrays;
+
 public class MagicBox<T> {
 
     T[] items;
 
     public MagicBox(int countItem ){
         T[] items = (T[]) new Object[countItem];
+        System.out.println("Array is: " + Arrays.deepToString(items));
     }
 
-    boolean add(T item){
-        for (int i = 0; i < items.length; i++) {
-            if(items[i] != null) {
-                items[i] = item;
+    public boolean add(T item){
+        for (int i = 0; i < items.length ; i++) {
+            if(this.items[i].equals(null)) {
+                this.items[i] = item;
                 return true;
             }
         }
-        System.out.println("Magic box is full");
         return false;
     }
 
-    //todo
-    public <T> T pick(T[] items, T answer1, T answer2){
+    //todo ((((
+    public <T> T pick(T[] items){
         for (int i = 0; i < items.length; i++) {
             if (items[i] == null) {
                 throw new IsNotFull();
