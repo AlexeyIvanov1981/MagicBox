0cb36e1db37128d9889b884b723215e8aa7c5acd
AlexeyIvanov1981 <lelikus81@yandex.ru>
2022-11-24 19:23:15 +0300

    Add magicBox class

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,13 +1,18 @@
-public class MagicBox <T>{
+public class MagicBox {
 
+    public int[] item;
 
+    public MagicBox(int countItem ){
+        this.item = new int[countItem];
+    }
 
-
-    boolean add(T item){
-
-
-
-
+    boolean add(int item){
+        for (int i = 0; i < this.item.length; i++) {
+            if(this.item[i] != 0){
+                this.item[i] = item;
+                return true;
+            }
+        }
         return false;
     }
 }
